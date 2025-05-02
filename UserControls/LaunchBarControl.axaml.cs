e5740803a4f56c9244f1eeb8e85c0e527c70f7cc
N4RSONIC <colorcatcc@outlook.com>
2025-05-03 00:56:41 +0800

    简易游戏启动实现，启动核心更新

--- UserControls/LaunchBarControl.axaml.cs
+++ UserControls/LaunchBarControl.axaml.cs
@@ -1,7 +1,10 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using System.Threading.Tasks;
+using TYMCL.Modules;
 
 namespace TYMCL.UserControls;
 
@@ -10,5 +13,10 @@ public partial class LaunchBarControl : UserControl
     public LaunchBarControl()
     {
         InitializeComponent();
+    }
+
+    private async void GameLaunchButton_Click(object sender, RoutedEventArgs e)
+    {
+        await GameTools.GameLaunchActive();
     }
 }
